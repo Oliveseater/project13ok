c0483c8f44ba71609330bbef552177cca5a3f3da
Oliveseater <nslivnicina@gmail.com>
2022-01-26 12:25:36 +0500

    first commit

---
+++ pj13/pj13/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите число а - ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b - ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("1 +\n2 -\n3 *\n4 /");
+int choose = Convert.ToInt32(Console.ReadLine());
+switch (choose)
+{
+    case 1:
+        Console.WriteLine("Ответ: " + (a + b));
+        break;
+    case 2:
+        Console.WriteLine("Ответ: " + (a - b));
+        break;
+    case 3:
+        Console.WriteLine("Ответ: " + (a * b));
+        break;
+    case 4:
+        Console.WriteLine("Ответ: " + (a / b));
+        break;
+    default: Console.WriteLine("wrong"); break;
+}
+Console.ReadKey();
