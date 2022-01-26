9086c8d2a16303319021c6ab23744ff7e6544d89
Invizible228 <Sulaymanov_tima@mail.ru>
2022-01-26 13:51:32 +0500

    div

--- pj13/pj13/Program.cs
+++ pj13/pj13/Program.cs
@@ -6,9 +6,10 @@ Console.WriteLine("1 +\n2 -\n3 *\n4 /");
 int choose = Convert.ToInt32(Console.ReadLine());
 switch (choose)
 {
-    case 3:
-        Console.WriteLine("Ответ: " + (a * b));
+     case 4:
+        Console.WriteLine("Ответ: " + (a / b));
         break;
+    
     default: Console.WriteLine("wrong"); break;
 }
 Console.ReadKey();
