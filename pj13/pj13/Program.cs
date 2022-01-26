05b87e6dd0ae400b68a0fc48ded28dd232789976
Invizible228 <Sulaymanov_tima@mail.ru>
2022-01-26 13:43:31 +0500

    Mult

--- pj13/pj13/Program.cs
+++ pj13/pj13/Program.cs
@@ -6,17 +6,8 @@ Console.WriteLine("1 +\n2 -\n3 *\n4 /");
 int choose = Convert.ToInt32(Console.ReadLine());
 switch (choose)
 {
-    case 1:
-        Console.WriteLine("Ответ: " + (a + b));
-        break;
-    case 2:
-        Console.WriteLine("Ответ: " + (a - b));
-        break;
     case 3:
         Console.WriteLine("Ответ: " + (a * b));
-        break;
-    case 4:
-        Console.WriteLine("Ответ: " + (a / b));
         break;
     default: Console.WriteLine("wrong"); break;
 }
